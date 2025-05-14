e77cf000c02b902dfd5334f9639d61b5f69ee27d
qadigitaldiscovery <qadigitaldiscovery@gmail.com>
2025-05-14 16:19:42 +0200

    s

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
-import { componentTagger } from "lovable-tagger";
+import { componentTagger } from 'lovable-tagger';
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [
-    react(),
+    react({
+      jsxImportSource: 'react',
+      jsxRuntime: 'automatic',
+    }),
     mode === 'development' && componentTagger(),
   ].filter(Boolean),
   build: {
