79c73f89b5ee3eb8e1e8a34d7a97e0cea8525f93
qadigitaldiscovery <qadigitaldiscovery@gmail.com>
2025-05-15 06:28:13 +0200

    v

--- src/components/layout/sidebar/SidebarNavList.tsx
+++ src/components/layout/sidebar/SidebarNavList.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { NavItem } from './types';
 
-export const SidebarNavList = ({ items = [] }: { items?: NavItem[] }) => (
-  <ul className="space-y-2">
-    {items.map((item) => (
-      <li key={item.href}>
-        <a href={item.href} className="block text-gray-700 dark:text-gray-200 hover:underline">
-          {item.label}
-        </a>
-      </li>
-    ))}
-  </ul>
-);
- 
+export const SidebarNavList = ({ items }: { items?: NavItem[] }) => {
+  const safeItems = items ?? [];
+  return (
+    <ul className="space-y-2">
+      {safeItems.map((item) => (
+        <li key={item.href}>
+          <a href={item.href} className="block text-gray-700 dark:text-gray-200 hover:underline">
+            {item.label}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+};
--- src/pages/DashboardV2.tsx
+++ src/pages/DashboardV2.tsx
@@ -24,4 +24,3 @@ export default function DashboardV2() {
     </PlatformLayout>
   );
 }
- 
