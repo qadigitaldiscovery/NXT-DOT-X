7fa4f51eb01768e9e2b4831995395aa85007ac53
gazbew <gambew@gmail.com>
2025-05-20 05:18:03 +0200

    x

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
 import React, { useEffect } from "react";
-import { useNavigate, BrowserRouter } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function App() {
-  let navigate;
-  try {
-    navigate = useNavigate();
-  } catch (error) {
-    // If useNavigate is used outside Router context, fallback to no-op
-    navigate = () => {};
-  }
+  const navigate = useNavigate();
 
   useEffect(() => {
     // Redirect to main dashboard
@@ -18,10 +12,4 @@ function App() {
   return null; // No need to render anything as we're redirecting
 }
 
-export default function AppWrapper() {
-  return (
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  );
-}
+export default App;
